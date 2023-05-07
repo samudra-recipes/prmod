2b2927d001effc297e589216252e7a87503eed3a
samudragogoi <50080120+samudragogoi@users.noreply.github.com>
2023-05-08 01:43:22 +0530

    new

--- app.js
+++ app.js
@@ -12,16 +12,16 @@
   };
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-//   const unsub = onSnapshot(doc(db, "app", "G8XLxyfOnP5zZoJIfErb"), (doc) => {
-//     console.log("Current data: ", doc.data().action);
-//     switch (doc.data().action) {
-//         case 0:
-//             console.log("0");
-//           break;
-//         case 13:
-//             activatePlayer()
-//       }
-// });
+  const unsub = onSnapshot(doc(db, "app", "G8XLxyfOnP5zZoJIfErb"), (doc) => {
+    console.log("Current data: ", doc.data().action);
+    switch (doc.data().action) {
+        case 0:
+            console.log("0");
+          break;
+        case 13:
+            focusZoom()
+      }
+});
 let action = async (nm) => {
     const docRef = doc(db, "app", "G8XLxyfOnP5zZoJIfErb")
     await updateDoc(docRef, {
@@ -35,14 +35,26 @@ function scrollToX(){
     console.log("scrolled")
 }
 function activatePlayer(){
-    const element = document.getElementsByClassName("player");
+    const elements = document.getElementsByClassName("player");
     let active = Math.floor(Math.random() * (4 - 0 + 1) + 0)
     console.log(active)
     const controls = document.createElement("div");
     controls.innerHTML = `<div class="touch" onclick="reverse(${active})">reverse</div><div class="touch" onclick="forward(${active})">forward</div>`
     document.body.appendChild(controls);
-    element[active].play()
-
+    elements[active].play()
+}
+function focusZoom(){
+    const scroll = document.getElementById("scroll")
+    scroll.style.display = 'none'
+    const element = document.getElementById("focus")
+    element.style.display = "block"
+    const img = document.getElementById("zoom")
+    for (let index = 0; index < 5000; index++) {
+        setTimeout(() => {
+            img.style.width = `${index}px`
+            img.style.height = `${index}px`
+          }, 3000);    
+    }
 }
 
 
